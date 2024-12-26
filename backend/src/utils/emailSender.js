923e75ad711cad63bd3979703e802a6d81c29890
YaadavAkhilesh <yadavakhil766@gmail.com>
2024-12-26 13:45:05 +0530

    Invoice Sending Done But Not Real GMAIL

--- backend/src/utils/emailSender.js
+++ backend/src/utils/emailSender.js
@@ -1,10 +1,11 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.ethereal.email',
+  port: 587,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
   }
 });
 
