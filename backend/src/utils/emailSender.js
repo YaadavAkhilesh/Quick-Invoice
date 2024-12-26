4b61aa1efd21e669f83aa760d909af93a119e42e
YaadavAkhilesh <yadavakhil766@gmail.com>
2024-12-26 14:08:25 +0530

    PDF send on GMAIL

--- backend/src/utils/emailSender.js
+++ backend/src/utils/emailSender.js
@@ -1,8 +1,9 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.ethereal.email',
-  port: 587,
+  service: 'gmail',
+  secure: true,
+  port: 465,
   auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
